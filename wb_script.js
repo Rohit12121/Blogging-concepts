38512d7fba53fe6f9d395b0b90c37e41c05ed8c7
Rohit-coder144 <rohitpal8588975034@gmail.com>
2022-05-25 20:51:09 +0530

    New features

---
+++ wb_script.js
@@ -0,0 +1,10 @@
+const navBar = document.querySelector('.bar-container')
+const navLinks = document.querySelector('.nav-links')
+const magnifying_glass = document.querySelector('.glass-style')
+
+
+
+// Add event listeners
+navBar.addEventListener('click', () => {
+    navBar.classList.toggle('cross-container')
+})
