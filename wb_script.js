def7b0331c2d85fa11e35dfc8a20a25da257abbe
Rohit-coder144 <rohitpal8588975034@gmail.com>
2022-05-26 12:26:36 +0530

    About us

--- wb_script.js
+++ wb_script.js
@@ -1,10 +1,40 @@
+// Query selectors
 const navBar = document.querySelector('.bar-container')
 const navLinks = document.querySelector('.nav-links')
 const magnifying_glass = document.querySelector('.glass-style')
 
 
+// ID's
+const searchContainer = document.getElementById('content-search-area')
+
+
+// Normal / cross bar result
+isBarClicked = false
+
+function navList() {
+    // Nav animation up to down
+    navLinks.classList.toggle('remove-margin')
+}
+
+// This function increase with of input
+function widthIncrease() {
+    searchContainer.style.transition = 'all 0.3s ease-in-out'
+    searchContainer.classList.add('remove-padding')
+}
+
+// Main function
+function storeFunc() {
+    isBarClicked = true
+    navBar.classList.toggle('cross-container')
+    navList()
+}
 
 // Add event listeners
-navBar.addEventListener('click', () => {
-    navBar.classList.toggle('cross-container')
-})
+navBar.addEventListener('click', storeFunc)
+document.onclick = function (e) {
+    if (e.target.id !== 'content-search-area') {
+        if (e.target.offsetParent && e.target.offsetParent.id !== 'content-search-area') {
+            searchContainer.classList.remove('remove-padding')
+        }
+    }
+}
